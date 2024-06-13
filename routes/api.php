4d4b851b7014523065b586cd855aea5971b6572b
SunTStudio <mahsunmuh0@gmail.com>
2024-06-13 09:35:08 +0700

    first

---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Produk;
+use App\Http\Controllers\Keranjang;
+use App\Http\Controllers\Transaksi;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/produk/all', [Produk::class, 'getListProduk']);
+Route::get('/produk/kode/{kode}', [Produk::class, 'getByKode']);
+Route::post('/produk/input', [Produk::class, 'store']);
+Route::put('/produk/input', [Produk::class, 'update']);
+Route::delete('/produk/hapus', [Produk::class, 'destroy']);
+
+Route::get('/keranjang/all', [Keranjang::class, 'getList']);
+Route::post('/keranjang', [Keranjang::class, 'store']);
+
+Route::get('/produk/dataTable', [Produk::class, 'dataDatables']);
+Route::get('/transaksi/dataTable', [Transaksi::class, 'dataDatables']);
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Produk;
+use App\Http\Controllers\Keranjang;
+use App\Http\Controllers\Transaksi;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [Produk::class, 'index']);
+Route::get('/produk', [Produk::class, 'index']);
+Route::get('/produk/form', [Produk::class, 'create']);
+Route::get('/produk/form/{id}', [Produk::class, 'edit']);
+Route::get('/keranjang', [Keranjang::class, 'index']);
+<<<<<<< HEAD
+Route::get('/checkout', function () {
+    return 'Dalam tahap pengembangan';
+   });;
+   
+=======
+
+Route::get('/checkout', [Transaksi::class, 'checkout']);
+Route::post('/checkout', [Transaksi::class, 'store']);
+
+Route::get('/transaksi', [Transaksi::class, 'index']);
+>>>>>>> ace4a4f9df4066a9b9149f35518121ebbf3e109c
